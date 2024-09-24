e2ae3bcaed3ace3aa22e81a3628cb20cfb69f1a0
André Brandão <75815203+brandao07@users.noreply.github.com>
2024-09-24 19:30:51 +0100

    fixed logs

--- golang-app-prom/main.go
+++ golang-app-prom/main.go
@@ -14,17 +14,11 @@ func main() {
 		Name: "db_backup_last_completion_timestamp_seconds",
 		Help: "The timestamp of the last successful completion of a DB backup.",
 	})
+	
 	completionTime.SetToCurrentTime()
 	if err := push.New(gatewayURL, "db_backup").
 		Collector(completionTime).
 		Grouping("db", "customers").
-		Push(); err != nil {
-		fmt.Println("Could not push completion time to Pushgateway:", err)
-	}
-
-	if err := push.New(gatewayURL, "db_backup").
-		Collector(completionTime).
-		Grouping("db", "products").
 		Push(); err != nil {
 		fmt.Println("Could not push completion time to Pushgateway:", err)
 	}
