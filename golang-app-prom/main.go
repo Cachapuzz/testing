84fadc7edaca5741f8defbcd2a9a8e99e2a245ad
DavidsonMatz <86520043+DavidsonMatz@users.noreply.github.com>
2024-10-11 18:01:01 +0100

    Prometheus communicates with Grafana

--- golang-app-prom/main.go
+++ golang-app-prom/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
@@ -14,12 +15,15 @@ func main() {
 		Name: "db_backup_last_completion_timestamp_seconds",
 		Help: "The timestamp of the last successful completion of a DB backup.",
 	})
-	
-	completionTime.SetToCurrentTime()
-	if err := push.New(gatewayURL, "db_backup").
-		Collector(completionTime).
-		Grouping("db", "customers").
-		Push(); err != nil {
-		fmt.Println("Could not push completion time to Pushgateway:", err)
+
+	for {
+		completionTime.SetToCurrentTime()
+		if err := push.New(gatewayURL, "db_backup").
+			Collector(completionTime).
+			Grouping("db", "customers").
+			Push(); err != nil {
+			fmt.Println("Could not push completion time to Pushgateway:", err)
+		}
+		time.Sleep(7 * time.Second)
 	}
 }
